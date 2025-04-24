de9f44c027ec36c0d92ad4589e5be507cf1b6b01
IlyaDyakonov <theApuoX@gmail.com>
2025-04-24 12:44:02 +0400

    third commit
    add a new page, bag fix backend and adminpanel

--- frontend/src/components/CRUD.tsx
+++ frontend/src/components/CRUD.tsx
@@ -4,6 +4,7 @@ import UseReg from "./SignUp/SignUp";
 import Page404 from './Page404/Page404';
 import AdminPanel from "./AdminPanelPage/AdminPanel";
 import { StartPages } from "./StartPage/StartPages";
+import { FacerAPI } from "./FacerAPI/FacerAPI";
 
 
 /**
@@ -16,6 +17,7 @@ function CRUD() {
                     <Route path="/" element={<StartPages />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<UseReg />} />
+                    <Route path="/facerapi" element={<FacerAPI />} />
                     <Route path="/admin" element={<AdminPanel />} />
                     <Route path="*" element={<Page404 />} />
                 </Routes>
